cdb2db28e23699a31dc23c563ef3a25d33a872e6
manojbaniya444 <86000102+manojbaniya444@users.noreply.github.com>
2024-01-19 16:30:07 +0545

    Project setup for frontend and backend

---
+++ Backend/readme.md
@@ -0,0 +1,2 @@
+# Flask backend
+> This is the backend of our hackathon project frontend where image classification model is served to the Frontend.
---
+++ Frontend/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-center'>App</div>
+  )
+}
+
+export default App
